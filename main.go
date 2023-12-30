779b6b8783176d3de56bd053393e5cb7749f596b
malik <malikabdulazizofcl@gmail.com>
2023-12-30 21:54:46 +0700

    configure port to serve

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/backent/fra-golang/helpers"
 	"github.com/backent/fra-golang/injector"
@@ -12,12 +14,19 @@ func main() {
 	err := godotenv.Load()
 	helpers.PanicIfError(err)
 
+	APP_PORT := os.Getenv("APP_PORT")
+	if APP_PORT == "" {
+		APP_PORT = "8080"
+	}
+
 	router := injector.InitializeRouter()
 
 	server := http.Server{
-		Addr:    "localhost:8080",
+		Addr:    "localhost:" + APP_PORT,
 		Handler: router,
 	}
+
+	fmt.Println("serving on localhost:" + APP_PORT)
 
 	server.ListenAndServe()
 }
